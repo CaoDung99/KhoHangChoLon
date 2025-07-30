d0e153528e40eee4d5c5b19e912e71bfff7f02b8
CaoDung99 <caotrandung99@gmail.com>
2025-07-30 14:39:02 +0700

    update

---
+++ js/loadpage.js
@@ -0,0 +1,11 @@
+$(window).on('load',function(){
+    $('#header').load('header.html');
+    $('#footer').load('footer.html');
+    $('#product').load('LoadProductSonThom.html');
+    $('#product2').load('LoadProductSonAcrylic.html');
+    $('#product3').load('LoadProductKeo.html');
+    $('#product4').load('LoadProductKimTuyen.html');
+    $('#product5').load('LoadProductDecal.html');
+    $('#product6').load('LoadProductGương.html');
+    // Gói header.html chứa và footer chứa
+});
